b5d3d414e2a343f28e95c9468dd64c8b719ae94b
George Violaris <george@hatchworksvc.com>
2023-01-17 13:10:20 +0200

    Changed tmux config

--- nvim/init.lua
+++ nvim/init.lua
@@ -157,6 +157,7 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', '<leader>m', '<Esc>:Neotree toggle<Return>')
 vim.keymap.set('n', '<leader>l', '<Esc><c-w>l')
 vim.keymap.set('n', '<leader>h', '<Esc><c-w>h')
+vim.keymap.set('n', '<leader>bd', '<Esc>:bprevious<bar>bdelete #<Return>')
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })

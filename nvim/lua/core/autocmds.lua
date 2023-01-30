4be44e4af7806dfc89900a9b423e2308faffcb88
George Violaris <george@hatchworksvc.com>
2023-01-30 21:01:47 +0200

    autocmd for neovim transparency in Alacritty

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -16,6 +16,23 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = function()
+    local highlights = {
+      'Normal',
+      'LineNr',
+      'Folded',
+      'NonText',
+      'SpecialKey',
+      'VertSplit',
+      'SignColumn',
+      'EndOfBuffer',
+      'TablineFill', -- this is specific to how I like my tabline to look like
+    }
+    for _, name in pairs(highlights) do vim.cmd.highlight(name .. ' guibg=none ctermbg=none') end 
+end, 
+})
+
 vim.api.nvim_create_autocmd('BufWinEnter', {
   pattern = '*',
   callback = function()
